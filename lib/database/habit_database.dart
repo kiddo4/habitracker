b018a8ee6fc74ba785d06c2824f442b58f9b098d
olanrewajutaiwo185 <olanrewajutaiwo185@gmail.com>
2024-04-23 15:38:01 +0000

    update

--- lib/database/habit_database.dart
+++ lib/database/habit_database.dart
@@ -1,1 +1,20 @@
-import
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:myapp/config/app_settings.dart';
+import 'package:myapp/models/habit.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HabitDatabase extends ChangeNotifier {
+
+  static late Isar isar;
+
+  static Future<void> init() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open(
+      [HabitSchema, AppSettingsSchema],
+       directory: dir.path);
+  }
+
+  
+  
+}
