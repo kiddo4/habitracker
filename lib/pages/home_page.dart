359ad1dcd88c7d7a86495c3fbda5d5d305819584
olanrewajutaiwo185 <olanrewajutaiwo185@gmail.com>
2024-04-25 01:36:31 +0000

    working on ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/config/theme/theme_provider.dart';
+import 'package:myapp/database/habit_database.dart';
+import 'package:myapp/models/habit.dart';
+import 'package:myapp/widgets/drawer_widget.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,18 +14,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final textController = TextEditingController();
+  
+  void creatNewHabit() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+          decoration: const InputDecoration(
+            hintText: 'Enter new habit',
+            ),
+        ),
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              textController.clear();
+            },
+            child: const Text('Cancel'),
+            ),
+          MaterialButton(
+            onPressed: () {
+              String newHabitName = textController.text;
+
+              context.read<HabitDatabase>().addHabit(newHabitName);
+             
+              Navigator.of(context).pop();
+
+              textController.clear();
+            },
+            child: const Text('Add'),
+            )
+        ],
+      )
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(),
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        child: CupertinoSwitch(
-          value: Provider.of<ThemeProvider>(context).isDarkMode,
-          onChanged: (value) => 
-            Provider.of<ThemeProvider>(context, listen: false).toggleTheme()
-          ),
-      ),
+      drawer: const MyDrawer(),
+      floatingActionButton: FloatingActionButton.extended(
+      onPressed: creatNewHabit,
+      elevation: 0,
+      backgroundColor: Theme.of(context).colorScheme.tertiary,
+      icon: const Icon(
+        Icons.add,
+        color: Colors.black,
+        ),
+      label: const Text(
+        'Add',
+        style: TextStyle(color: Colors.black,),
+        ),
+      )
     );
+    
   }
 }
